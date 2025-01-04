0ee23971aa5189dfebec7cf8f43501146da1944f
Ganesh Joshi <ganesh@kitchen365.com>
2025-01-04 23:07:12 +0530

    Updated path for production environment

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const path = require('path');
 const mongoose = require('mongoose');
 const cookieSession = require('cookie-session');
 const passport = require('passport');
@@ -26,19 +27,17 @@ app.use(passport.session());
 require('./routes/authRoutes')(app);
 require('./routes/billingRoutes')(app);
 
-if(process.env.NODE_ENV === 'production'){
-    /* 
-        Express will serve up production assets like our main.js file, or main.css file
-    */
-    app.use(express.static('client/build'));
+// Serve static files in production
+if (process.env.NODE_ENV === 'production') {
+    // Serve static files from the React app
+    app.use(express.static(path.join(__dirname, 'client', 'build')));
 
-    /*
-        Express will serve up the index.html file if it doesn't recognize the route
-    */
-    
-    const path = require('path');
-
+    // Catch-all handler for React routes
+    app.get('*', (req, res) => {
+        res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+    });
 }
+
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT);
